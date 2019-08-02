6dbfcd63bee89a039586999253ae562522d13777
Sarah Higgins <higginssarah12@gmail.com>
2019-08-02 18:01:24 -0400

    Julia serial code

--- primeSerial.c
+++ primeSerial.c
@@ -2,28 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "myheader.h"
+#define JULIA_ENABLE_THREADING
 #include <julia.h>
-//#define JULIA_ENABLE_THREADING
 
 int main(int argc, char** argv){
 
     jl_init();
 
-    int n=10000000;
+    int n=100;
+    //jl_value_t *n = jl_eval_string("10000000");
     int numprimes = 0;
     int i;
 
-    jl_eval_string("print(sqrt(2.0))");
+    //jl_value_t *ret = jl_eval_string("sqrt(2.0)");
 
-//    for (i = 1; i <= n; i++)
-//    {
-//        if (is_prime(i) == 1)
-//            numprimes ++;
-//    }
+    for (i = 1; i <= n; i++)
+    {
+        if (is_prime(i) == 1)
+            numprimes ++;
+    }
 
-//    printf("Number of Primes: %d\n",numprimes);
+    printf("Number of Primes: %d\n",numprimes);
 
     jl_atexit_hook(0);
+
+    return 0;
 
 }
 

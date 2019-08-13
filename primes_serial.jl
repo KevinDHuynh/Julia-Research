ee83aaba33be6b81d8ebc897fb40a0398df898b1
Sarah Higgins <higginssarah12@gmail.com>
2019-08-13 16:28:44 -0400

    commenting serial code

--- primes_serial.jl
+++ primes_serial.jl
@@ -1,8 +1,12 @@
 #!/opt/sw/julia-1.1.1/bin/julia
 
+# declare size n and number of primes
 n=30000000;
 numprimes = 0;
 
+
+# funtion is_prime determines if a function is prime or not.
+#	if it is, then a 1 is returned.
 function is_prime(n)
 	if n == 0 | n == 1
 		return 0
@@ -18,13 +22,15 @@ function is_prime(n)
 	return 1
 end
 
-
+# if a 1 is returned from function is_prime(), then this for
+#	loop runs to increment numprimes to show how many prime
+#	numbers there are within the number n.
 for i = 1:n
 	if is_prime(i) == 1
 		global numprimes += 1
 	end
 end
 
-
+# Print out the number of primes to the user
 println("Number of Primes: ")
 println(numprimes)
